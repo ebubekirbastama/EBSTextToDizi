31a1add211edd59959e62047e8a217780634edca
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2021-01-01 18:23:37 +0300

    Add files via upload

---
+++ Text to Dizi/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Text_to_Dizi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.eBSTimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.diziOluşturToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(386, 409);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.ContextMenuStrip = this.contextMenuStrip1;
+            this.richTextBox2.Location = new System.Drawing.Point(422, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(386, 409);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eBSTimeToolStripMenuItem,
+            this.diziOluşturToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // eBSTimeToolStripMenuItem
+            // 
+            this.eBSTimeToolStripMenuItem.Enabled = false;
+            this.eBSTimeToolStripMenuItem.Name = "eBSTimeToolStripMenuItem";
+            this.eBSTimeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.eBSTimeToolStripMenuItem.Text = "EBS Time";
+            // 
+            // diziOluşturToolStripMenuItem
+            // 
+            this.diziOluşturToolStripMenuItem.Name = "diziOluşturToolStripMenuItem";
+            this.diziOluşturToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.diziOluşturToolStripMenuItem.Text = "Dizi Oluştur";
+            this.diziOluşturToolStripMenuItem.Click += new System.EventHandler(this.diziOluşturToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(820, 433);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text to Dizi İşlemleri";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem eBSTimeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem diziOluşturToolStripMenuItem;
+    }
+}
+
---
+++ Text to Dizi/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_to_Dizi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string dizitext = "string[] dizii =";
+        string[] parantez = { "{", "};" };
+        private void diziOluşturToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var dizi = richTextBox1.Text.Replace("\n","~");
+            richTextBox2.AppendText(dizitext);
+            richTextBox2.AppendText(parantez[0]);
+            for (int i = 0; i < dizi.Split('~').Length; i++)
+            {
+
+                if (dizi.Split('~').Length - 1 != i) //EBS Coding...
+                {
+                    richTextBox2.AppendText("\"" + dizi.Split('~')[i].ToString() + "\"" + ",");
+                }
+                else
+                {
+                    richTextBox2.AppendText("\"" + dizi.Split('~')[i].ToString() + "\"");
+                }
+
+            }
+
+            richTextBox2.AppendText(parantez[1]);
+
+        }
+    }
+}
